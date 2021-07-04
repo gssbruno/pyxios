1492ee4e0e807660d495979ad889de49064731b2
brunogss <b.g.santos@outlook.com>
2021-07-03 23:26:48 -0300

    Alteracao no nome do package pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='pyxios-bgss',
+    name='pyxios',
     version="1.0.0",
     author="Bruno Santos",
     author_email="b.g.santos@outlook.com",
